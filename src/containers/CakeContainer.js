454cad0c854fb964f96db1358421f58e6d229b99
Hanqing Huang <h.hanqing@yahoo.com>
2022-10-03 15:57:53 +0100

    add cakes as state

--- src/containers/CakeContainer.js
+++ src/containers/CakeContainer.js
@@ -1,1 +1,24 @@
 import {useState} from "react";
+
+const [cakes, setCakes] = useState([
+    {
+        cakeName: "Lemon Drizzle",
+        ingredients: ["eggs", "butter", "lemon  zest", "sugar", "self-raising flour"],
+        rating: 5
+    },
+    {
+        cakeName: "Tea Loaf",
+        ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+        rating: 3
+    },
+    {
+        cakeName: "Brownie",
+        ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+        rating: 4
+    },
+    {
+        cakeName: "Carrot Cake",
+        ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+        rating: 5
+    }
+]);
